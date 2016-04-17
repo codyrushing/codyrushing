0efe80e3e71bec96b6054b4dc2eda68154608ee8
Cody Rushing <codyrushing@gmail.com>
2016-04-17 11:44:53 +1000

    improving section width strategy

--- public/demos/maternity-leave/src/app/components/visualizer.js
+++ public/demos/maternity-leave/src/app/components/visualizer.js
@@ -6,15 +6,15 @@ var visualizer = {
     this.root = d3.select(element)
     this.build()
     this.loadData(url, (data) => {
-      this.dataset = data
+      this.dataset = data.filter((node, i) => {
+        return node.paidLeave !== null
+      })
+      console.log(this.dataset)
       this.draw(this.dataset)
     })
     return this
   },
-  constants: {
-    DAMPER: 0.102
-  },
-  build: function({width=600, height=400, margins={top:50, right:50, bottom:50, left:50}}={}){
+  build: function({width=960, height=600, margins={top:50, right:50, bottom:50, left:50}}={}){
     this.width = width
     this.svg = this.root.append("svg")
       .attr({
@@ -73,29 +73,38 @@ var visualizer = {
     return Math.sqrt(weeks)
   },
   processSectionsByPaid: function(sectionsByPaid){
-    var totalWeeks = this.reduceTotalWeeks(this.dataset)
+    // width of sections are based upon their largest cluster
+    // so we first extract all of the largest clusters
+    var maxClusters = sectionsByPaid.map((section, i) => {
+      var max, maxValue = 0;
+      section.subsectionsByUnpaid = this.nestedStructure("unpaidLeave").entries(section.values).reverse()
+      section.subsectionsByUnpaid.forEach((subsection) => {
+        var subsectionWeeks = this.reduceTotalWeeks(subsection.values)
+        if (subsectionWeeks > maxValue) {
+          maxValue = subsectionWeeks
+          max = subsection
+        }
+      })
+      return max
+    })
+
+    var totalMaxClusterWeeks = this.reduceTotalWeeks(
+      maxClusters.reduce((a,b) => {
+        return a.concat(b.values)
+      }, [])
+    )
 
     sectionsByPaid.forEach((section,i) => {
-      let prev = sectionsByPaid[i-1] || {x0: 0, x1:0}
-      section.totalWeeks = this.reduceTotalWeeks(section.values)
-      section.width = section.totalWeeks / totalWeeks * this.width
+      var prev = sectionsByPaid[i-1] || {x0: 0, x1:0}
+      // use the number of weeks in our max cluster to determine width of section
+      section.width = this.reduceTotalWeeks(maxClusters[i].values) / totalMaxClusterWeeks * this.width
       section.x0 = prev.x1
       section.x1 = section.x0 + section.width
-
-      // for debugging
-      // this.mainGroup.append("rect")
-      //   .attr({
-      //     x: section.x0,
-      //     y: 0,
-      //     width: section.width,
-      //     height: 200,
-      //     fill: "blue"
-      //   })
 
       // begin building out the clusters for each section
       this.processSubsectionsByUnpaid(
         section,
-        this.nestedStructure("unpaidLeave").entries(section.values).reverse()
+        section.subsectionsByUnpaid
       )
 
     })
@@ -104,7 +113,7 @@ var visualizer = {
     var yOffset = 0
     subsectionsByUnpaid.forEach((subsection, i) => {
       var radius = this.weeksToRadius(this.reduceTotalWeeks(subsection.values)),
-          y = yOffset + radius/2,
+          y = yOffset,
           clusterData = {
             radius: radius,
             x: section.x0 + (section.x1 - section.x0)/2 + Math.random(),
@@ -125,9 +134,6 @@ var visualizer = {
 
     })
   },
-  charge: function(d){
-    return -Math.pow(d.radius, 2.0) / 8;
-  },
   draw: function(){
     this.scale.x
       .domain( d3.extent(this.dataset.map(this.xAccessor)) )
@@ -140,35 +146,28 @@ var visualizer = {
         .nodes(this.dataset)
         .size([this.width, this.height])
         .gravity(0)
-        .friction(0.9)
-        .charge(this.charge)
+        .charge(0)
         .on("tick", (e) => {
           return this.onTick(e)
         })
 
-    force.start()
-
-    setTimeout(() => {
-      // console.log("stopping")
-      // force.stop()
-    }, 10000)
-
-    console.log(this.dataset)
-
     this.bubbles = this.mainGroup.selectAll("circle")
       .data(this.dataset)
       .enter().append("circle")
-      .attr('r', 0)
       .attr('stroke-width', 1)
       .attr("stroke", "red")
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; })
       .attr("fill", "none")
-      .attr('r', function (d) { return d.radius; });
+      .attr('paid', function (d) { return d.paidLeave; })
+      .attr('unpaid', function (d) { return d.unpaidLeave || "null"; })
+      .attr('name', function(d) { return d.name; });
 
-    this.bubbles.transition()
-      .duration(1000)
-      .attr('r', function (d) { return d.radius; });
+    force.start()
+
+    for(var i=100; i>0; i--){
+      force.tick()
+    }
+
+    force.stop()
 
 
     // Run the layout a fixed number of times.
@@ -178,18 +177,64 @@ var visualizer = {
     // this.axisGroup.x.call(this.axis.x)
   },
   onTick: function(e) {
-    console.log(e)
+    console.log(this)
     this.bubbles
-      .each(this.moveTowardCluster(e.alpha))
+      // .each(this.bounceBack(e.alpha))
+      .each(this.moveTowardCluster.call(this, e.alpha))
+      .each(this.collide.call(this, 0.5))
       .attr("cx", function(d) { return d.x; })
       .attr("cy", function(d) { return d.y; })
+      .attr('r', function (d) { return d.radius; })
   },
   moveTowardCluster: function(alpha){
-    var damper = 0.102
+    var damper = 0.202
     return (d) => {
-      d.x = d.x + (d.cluster.x - d.x) * damper * alpha * 1.1
-      d.y = d.y + (d.cluster.y - d.y) * damper * alpha * 1.1
+      d.x = d.x + (d.cluster.x - d.x) * damper * alpha
+      d.y = d.y + (d.cluster.y - d.y) * damper * alpha
     }
+  },
+  bounceBack: function(alpha){
+    var ctx = this
+    return (d) => {
+      if (d.x < 0 || d.x > ctx.width){
+        let vX = d.x - d.px
+        d.x = d.x - vX
+      }
+      if (d.y < 0 || d.y > ctx.height){
+        let vY = d.y - d.py
+        d.y = d.y - vY
+      }
+    }
+  },
+  collide: function(alpha){
+    var padding = 0.5, // separation between same-color nodes
+    clusterPadding = 5, // separation between different-color nodes
+    maxRadius = this.weeksToRadius(52);
+
+    var quadtree = d3.geom.quadtree(this.dataset);
+    return function(d) {
+      var r = d.radius + maxRadius + Math.max(padding, clusterPadding),
+          nx1 = d.x - r,
+          nx2 = d.x + r,
+          ny1 = d.y - r,
+          ny2 = d.y + r;
+      quadtree.visit(function(quad, x1, y1, x2, y2) {
+        if (quad.point && (quad.point !== d)) {
+          var x = d.x - quad.point.x,
+              y = d.y - quad.point.y,
+              l = Math.sqrt(x * x + y * y),
+              r = d.radius + quad.point.radius + (d.cluster === quad.point.cluster ? padding : clusterPadding);
+          if (l < r) {
+            l = (l - r) / l * alpha;
+            d.x -= x *= l;
+            d.y -= y *= l;
+            quad.point.x += x;
+            quad.point.y += y;
+          }
+        }
+        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+      });
+    };
   },
   loadData: function(url, done){
     if(!url) throw new Error("Data url must be provided")
