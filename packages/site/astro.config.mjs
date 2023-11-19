796a40792a86389240d20f5a36651489369f8ff7
Cody Rushing <codyrushing@gmail.com>
2023-11-19 14:12:45 -0600

    removing preact

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import preact from '@astrojs/preact';
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), preact(), compress()]
+  integrations: [tailwind(), image(), compress()]
 });
